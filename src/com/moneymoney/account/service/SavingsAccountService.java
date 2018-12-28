601bd75d0ea84d687dccbf399e5b72c3153f0f13
Parvatham <saikiran.parvatham@capgemini.com>
2018-12-28 13:07:22 +0530

    adding assignment

---
+++ src/com/moneymoney/account/service/SavingsAccountService.java
@@ -0,0 +1,43 @@
+package com.moneymoney.account.service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.moneymoney.account.SavingsAccount;
+import com.moneymoney.exception.AccountNotFoundException;
+
+public interface SavingsAccountService {
+
+	SavingsAccount createNewAccount(String accountHolderName, double accountBalance, boolean salary) throws ClassNotFoundException, SQLException;
+
+	SavingsAccount updateAccount(SavingsAccount savingsAccount) throws ClassNotFoundException, SQLException;
+
+	SavingsAccount getAccountById(int accountNumber) throws ClassNotFoundException, SQLException, AccountNotFoundException;
+
+	SavingsAccount deleteAccount(int accountNumber) throws ClassNotFoundException, SQLException;
+	
+	List<SavingsAccount> getAllSavingsAccount() throws ClassNotFoundException, SQLException;
+
+	void fundTransfer(SavingsAccount sender, SavingsAccount receiver, double amount) throws ClassNotFoundException, SQLException;
+	void deposit(SavingsAccount account, double amount) throws ClassNotFoundException, SQLException;
+	void withdraw(SavingsAccount account, double amount) throws ClassNotFoundException, SQLException;
+
+	List<SavingsAccount> getAllSavingsAccountsSortedByNames() throws ClassNotFoundException, SQLException;
+
+	List<SavingsAccount> getAllSavingsAccountsSortedByRange(double minimum,
+			double maximum) throws ClassNotFoundException, SQLException;
+
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
